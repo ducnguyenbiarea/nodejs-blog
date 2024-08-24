89f355a71100d619a5fb756d3afa43572617a8d1
Duc20225812 <nguyentriduc92004@gmail.com>
2024-08-24 21:57:36 +0700

    mvc

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+class NewsController {
+    // [get] /news
+    index(req, res) {
+        res.render('news');
+    }
+
+    // [get] /news/:slug
+    show(req, res){
+        res.send('NEWS DETAILS!!!')
+    }
+}
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@ const express = require('express')
 const path = require('path');
 const app = express();
 const port = 3000;
-
-app.use(express.static(path.join(__dirname, 'public')))
+const route = require('./routes');
 // HTTP logger
 const morgan = require('morgan');
 const {engine} = require('express-handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan('combined'));
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
 // Template engine
 app.engine('hbs', engine({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-app.use(morgan('combined'));
-app.get('/', (req, res) => {
-  res.render('home');
-})
+
+// routes init
+route(app);
 
 app.listen(port, () => 
   console.log(`Example app listening at port http://localhost:${port}`));
